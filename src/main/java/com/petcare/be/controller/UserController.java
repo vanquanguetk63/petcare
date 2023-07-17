239c677aff6de6e856c63d79b40e1610f5cbb642
quang.tran41 <quang.tran4@mservice.com.vn>
2023-07-17 18:02:48 +0700

    add logger

--- src/main/java/com/petcare/be/controller/UserController.java
+++ src/main/java/com/petcare/be/controller/UserController.java
@@ -2,6 +2,8 @@ package com.petcare.be.controller;
 
 import com.petcare.be.model.User;
 import com.petcare.be.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ public class UserController {
 
     @GetMapping("/api/users")
     public List<User> getUsers() {
+
         return userRepository.findAll();
     }
 }
